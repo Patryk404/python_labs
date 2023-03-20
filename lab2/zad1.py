e8540286c6d8ced0e2294342bc4fd12a3b983eff
Patryk404 <patryk744@op.pl>
2023-03-20 09:38:45 +0100

    Finished exercise 1 lab2

---
+++ lab2/zad1.py
@@ -0,0 +1,21 @@
+ 
+arr = [] 
+string = ""
+changed_string = ""
+for i in range(500,3000):
+    if(i%7==0 and i%5!=0):
+        arr.append(i)
+ 
+for element in arr:
+    string += str(element)
+
+
+for i in range(len(string)):
+    if string[i] == "2" and string[i+1] == "1":
+        changed_string +='X' 
+        changed_string +='X'
+    else:
+        changed_string+=string[i]
+
+print(string)
+print(changed_string)
