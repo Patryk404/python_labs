a0418859d74e831b74aa8c7da3929130d68c9102
Patryk404 <patryk744@op.pl>
2023-03-21 23:44:09 +0100

    Added zad3

---
+++ lab2/zad3.py
@@ -0,0 +1,26 @@
+import re 
+import time
+
+print("Type Something: ")
+text = input()
+
+words = re.findall(r'\w+',text)
+
+if len(words) != 1:
+    print("You should pass only one word!") 
+else:
+    correct_word = ""
+    text.lower()
+    start_time = time.time() 
+    file = open('SJP.txt')
+    sjp = file.read().splitlines()
+    # print(sjp)
+    for word in sjp:
+        if word == text: 
+            correct_word = word
+    if len(correct_word):
+        print("word correct with sjp")
+        print("--- %s seconds ---" %(time.time() - start_time))
+    else:
+        print("word not correct with sjp")
+        print("--- %s seconds ---" %(time.time() - start_time))
